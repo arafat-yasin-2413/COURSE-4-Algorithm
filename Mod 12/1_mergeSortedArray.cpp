1d909d950e50e3748c239d5df721930ed6379f95
arafat-yasin-2413 <129652012+arafat-yasin-2413@users.noreply.github.com>
2023-08-21 11:21:54 +0600

    first commit

--- Mod 12/1_mergeSortedArray.cpp
+++ Mod 12/1_mergeSortedArray.cpp
@@ -1,17 +1,221 @@
+
+// Merge two sorted arrays
 #include<bits/stdc++.h>
 using namespace std;
 
+void printArray(int ar[],int sz)
+{
+    for(int i = 0; i<sz ; i++)
+    {
+        cout<<ar[i]<<" ";
+    }
+    cout<<endl; 
+}
+
+void mergeSortedArrays(int arr[],int l, int m, int r)
+{
+    int leftSize = m-l+1;
+    int L[leftSize];
+    
+    for(int i = l,k = 0; i<=m; i++,k++)
+    {
+        L[k] = arr[i];
+    }
+    // printArray(L,leftSize);
+
+
+    int rightSize = r-m;
+    int R[rightSize];
+    for(int i = m+1, k = 0; i<=r;i++,k++)
+    {
+        R[k] = arr[i];
+    }
+    // printArray(R,rightSize);
+
+    int i = 0, j = 0, k = 0; 
+    while(i<leftSize and j<rightSize)
+    {
+        if(L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+
+        else 
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    // cout<<k<<endl;
+    while(i<leftSize)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while(j<rightSize)
+    {
+        arr[k] = R[j];
+        j++;
+        k++; 
+    }
+
+
+}
+
 int main()
 {
-    // This is merge sort 
     int n; 
-    cin >> n ; 
+    cin >>n; 
     int a[n];
-
     for(int i = 0; i<n; i++)
     {
-        cin >> a[i]; 
+        cin>>a[i];
     }
+
+
+    // printArray(a,n); 
+
+    mergeSortedArrays(a,0,4,n-1);
+    printArray(a,n);
+
+
+
+    
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+// Rahat Khan Pathan vaiya
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void merge(int a[],int l,int m,int r)
+{
+    int leftSize = m-l+1;
+    int rightSize = r-m;
+    int L[leftSize],R[rightSize];
+    int k=0;
+    for(int i=l;i<=m;i++)
+    {
+        L[k]=a[i];
+        k++;
+    }
+    k=0;
+    for(int i=m+1;i<=r;i++)
+    {
+        R[k]=a[i];
+        k++;
+    }
+    int i=0,j=0;
+    int cur=l;
+    while(i<leftSize && j<rightSize)
+    {
+        if(L[i] <= R[j])
+        {
+            a[cur]=L[i];
+            i++;
+        }
+        else 
+        {
+            a[cur]=R[j];
+            j++;
+        }
+        cur++;
+    }
+
+    while(i<leftSize)
+    {
+        a[cur]=L[i];
+        i++;
+        cur++;
+    }
+
+    while(j<rightSize)
+    {
+        a[cur]=R[j];
+        j++;
+        cur++;
+    }
+}
+void merge_sort(int a[],int l,int r)
+{
+    if(l<r)
+    {
+        int mid=(l+r)/2;
+        merge_sort(a,l,mid);
+        merge_sort(a,mid+1,r);
+        merge(a,l,mid,r);
+        // cout<<"This"<<endl;
+        // for(int i=l;i<=mid;i++)
+        // {
+        //     cout<<a[i]<<" ";
+        // }
+        // cout<<endl;
+        // for(int i=mid+1;i<=r;i++)
+        // {
+        //     cout<<a[i]<<" ";
+        // }
+        // cout<<endl;
+
+    }
+}
+int main()
+{
+    int n; 
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    merge_sort(a,0,n-1);
+    // for(int i=0;i<n;i++)
+    // {
+    //     cout<<a[i]<<" ";
+    // }
+
+
+    for(int i = 0; i<n; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    cout<<endl; 
+    return 0;
+}
+*/
