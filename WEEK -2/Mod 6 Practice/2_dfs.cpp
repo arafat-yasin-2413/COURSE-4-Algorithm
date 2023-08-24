e3f0a5baa897f192931d1f9f8736b853e073bc37
arafat-yasin-2413 <129652012+arafat-yasin-2413@users.noreply.github.com>
2023-08-24 13:01:02 +0600

    main

--- WEEK -2/Mod 6 Practice/2_dfs.cpp
+++ WEEK -2/Mod 6 Practice/2_dfs.cpp
@@ -1,6 +1,90 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+const int N = 1e5+8;
+vector<int> adj[N];
+bool visited [N];
+
+void dfs(int u )
+{
+    visited[u] = true;
+    cout<<u<<" ";
+    for(int v : adj[u])
+    {
+        if(visited[v]) continue;
+        dfs(v);
+    }
+}
+
+int main()
+{
+    int n, m  ;
+    cin >> n >> m ;
+    for(int i = 1;i<=m; i++)
+    {
+        int u, v; 
+        cin >> u >> v; 
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    // for(int i = 1; i<=n; i++)
+    // {
+    //     cout<<"List "<<i<<": ";
+    //     for(int v : adj[i])
+    //     {
+    //         cout<<v<<" ";
+    //     }
+    //     cout<<endl; 
+    // }
+
+    dfs(1);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+#include<bits/stdc++.h>
+using namespace std;
+
 const int N = 1e6+7;
 vector<int> adj[N];
 bool visited[N];
@@ -55,3 +139,4 @@ int main()
 
     return 0;
 }
+*/
