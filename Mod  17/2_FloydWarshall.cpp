eb5fb99db9b2ccc5e38c89aa4a4309991117547e
arafat-yasin-2413 <129652012+arafat-yasin-2413@users.noreply.github.com>
2023-09-01 17:59:28 +0600

    Module 17 Uploaded

--- Mod  17/2_FloydWarshall.cpp
+++ Mod  17/2_FloydWarshall.cpp
@@ -39,7 +39,7 @@ int main()
         {
             for(int j = 1; j<=n; j++)
             {
-                dist[i][j] = min(dist[i][k]+dist[k][j], dist[i][j]);
+                dist[i][j] = min(dist[i][j],dist[i][k]+dist[k][j]);
             }
         }
     }
