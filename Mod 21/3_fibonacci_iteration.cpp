34c1b585b658f29c64f4f5ac1dba1885bed2b5e5
arafat-yasin-2413 <129652012+arafat-yasin-2413@users.noreply.github.com>
2023-09-13 15:05:01 +0600

    Module 21 Uploaded

--- Mod 21/3_fibonacci_iteration.cpp
+++ Mod 21/3_fibonacci_iteration.cpp
@@ -1,29 +1,58 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// #define ll long long
+
+// int main()
+// {
+//     ll n; 
+//     cin >> n; 
+
+//     ll fibo[n+1]; 
+//     fibo[0] = 1; 
+//     fibo[1] = 1; 
+
+//     for(int i = 2; i<=n; i++)
+//     {
+//         fibo[i] = fibo[i-1]+fibo[i-2];
+//     }
+
+//     // for(int i = 0; i<=n;  i++)
+//     // {
+//     //     cout<<fibo[i]<<" "; 
+//     // }
+//     cout<<fibo[100]<<endl;
+//     // cout<<min(30,30)<<endl;
+
+
+//     return 0;
+// }
+
 #include<bits/stdc++.h>
 using namespace std;
 
 #define ll long long
-
 int main()
 {
-    ll n; 
+    int n; 
     cin >> n; 
 
-    ll fibo[n+1]; 
+    ll fibo[n+1];
     fibo[0] = 1; 
     fibo[1] = 1; 
 
-    for(int i = 2; i<=n; i++)
+    for(int i =2; i<=n; i++)
     {
-        fibo[i] = fibo[i-1]+fibo[i-2];
+        fibo[i] = fibo[i-1]+fibo[i-2]; 
     }
 
-    // for(int i = 0; i<=n;  i++)
+    // for(int i = 0; i<=n; i++)
     // {
-    //     cout<<fibo[i]<<" "; 
+    //     cout<<fibo[i]<<" ";
     // }
-    cout<<fibo[100]<<endl;
-    // cout<<min(30,30)<<endl;
+    // cout<<endl; 
 
+    cout<< fibo[n] <<endl;
 
     return 0;
 }
